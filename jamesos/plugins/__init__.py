11b6d48b159c1d574fa1b3dc764e7b74f149917b
thedorfer <thedorfer@gmail.com>
2026-09-10 11:35:36 -0500

    Add plugins namespace

---
+++ jamesos/plugins/__init__.py
@@ -0,0 +1,5 @@
+"""Plugin namespace for JamesOS.
+
+Plugins will add higher-level platform capabilities such as WordPress,
+Nextcloud, backup orchestration, Cloudflare, media services, and AI workflows.
+"""
